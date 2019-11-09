6b0b214d33ceaa826886a8a19a03eb8f2e2c84f6
William St. Onge <stongew1@mymail.nku.edu>
2019-11-08 19:38:48 -0500

    communist

--- Assignment4/increasingsequence.c
+++ Assignment4/increasingsequence.c
@@ -3,7 +3,7 @@
 	
 	Author: Will St. Onge
 
-	Description: Computes an increasing sequence of prime numbers between the starting and ending number inputted by the user. The algorithm for find the next number in the sequence and print it out is written in x86 assembly. It starts at the first input number and checks if that is prime. If it is prime, it will output that number to console and add that to the accumulator and current equal to it. If it is not prime, it will increment the current number by 1. Once the end number is reached, the program will stop.
+	Description: Computes an increasing sequence of prime numbers between the starting and ending number inputted by the user using Intel x86 assembly. The algorithm for find the next number in the sequence and print it out is written in x86 assembly. It starts at the first input number and checks if that is prime. If it is prime, it will output that number to console and add that to the accumulator and current equal to it. If it is not prime, it will increment the current number by 1. Once the end number is reached, the program will stop.
 */
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -27,84 +27,50 @@ int main()
 	printf("Insert a maximum number: ");
 	scanf("%d", &end);
 
-	// Check that end is greater than start
-	if (end <= start)
-	{
-		printf("\nStarting number must be less than the maximum number");
-		return 1;
-	}
-
-	// Set the current number to start
-	current = start;
-
 	__asm
 	{
-	top:						; Top of the program (main loop).
-
-		mov eax, current		; Move the current number into eax.
-		
-	inner:						; Inner loop of the program.
-		
-		cmp eax, end			; If the current number is >= the end number, exit the program.
+		mov eax, start			// Initialize the current number to the starting number.
+		mov current, eax
+		cmp eax, end			// Test if start is greater than end, if it is then the inputs are invalid and we need to exit the program.
+		jg breakout
+	top:						
+		mov eax, current		// Move the current number into eax so we can work with it.
+	inner:						
+		cmp eax, end			// If the current number is >= the end number, exit the program since we are outside of the range.
 		jge breakout
-
-		jmp isPrime				; Jump to the isPrime routine to test primality of current.
-
-	returnpoint:				; Return point for the isPrime code.
-
-		cmp ret, 1				; If the return value is a 0, the number is prime so continue to the if logic.
+		jmp isPrime				// Jump to the isPrime routine to test primality of current.
+	returnpoint:				
+		cmp ret, 1				// If the return value is a 0, the number is prime so continue to the if logic.
 		je condition
-
-		inc current				; Number is not a prime, increment current and see if that number is prime.
+		inc current				// Number is not a prime, increment current and see if that number is prime.
 		jmp inner
-
-	condition:					; If the number is prime, this get ran.
-
-		push current			; Pushes format and data onto the stack.
+	condition:					
+		push current			// Pushes format and data onto the stack for printf call.
 		push str
-
-		call printf				; Call the printf function.
-
-		pop ecx					; Cleanup the stack.
+		call printf				// Call the printf function to print out the next number.
+		pop ecx					// Cleanup the stack.
 		pop ecx
-
-		mov eax, current		; Move current into the eax register so it can be added to acc.
-
-		add acc, eax			; Increment acc by current.
-
-		mov ebx, acc			; Set current equal to acc + 1.
+		mov eax, current		// Move current into the eax register so it can be added to acc.
+		add acc, eax			// Increment acc by current so we know where to start in the next iteration.
+		mov ebx, acc			// Set current equal to acc + 1 so we know where to start in the next iteration.
 		inc ebx
 		mov current, ebx
-
-		jmp top					; Loop back up to the top of the program.
-		
-	isPrime:					; Check if a number is prime.
-
-		mov ebx, 2				; Initialize the divisor to 2
-
-	primeLoop:					; loop for the isPrime logic
-
-		mov eax, current		; Confirm eax is still current.
-
-		cmp ebx, eax			; Make sure the current 'index' is less than the number.
+		jmp top					// Loop back up to the top of the program so we can continue the loop.
+	isPrime:					
+		mov ebx, 2				// Initialize the divisor to 2
+	primeLoop:					// loop for the isPrime logic
+		mov eax, current		// Confirm eax is still current since were using the div operator.
+		cmp ebx, eax			// Make sure the current 'index' is less than the number so we dont divide the number by itself. If it is, go back to the main part of the program.
 		jge returnpoint
-
-		mov edx, 0				; Make sure edx is 0 so we don't get modulo inaccuracy.
-		mov ret, 0				; Reset ret to 0 so the routine doesn't return 1 when it shouldn't.
-
-		div ebx					; Divide eax by ebx. This puts the result in eax and the remainder in edx.
-
-		cmp edx, 0				; If the remainder is 0, then current is not prime. Escape from the isPrime logic and go back to the main program.
+		mov edx, 0				// Make sure edx is 0 so we don't get modulo inaccuracy.
+		mov ret, 0				// Reset ret to 0 so the routine doesn't return 1 when it shouldn't.
+		div ebx					// Divide eax by ebx. This puts the quotient into eax and the remainder in edx.
+		cmp edx, 0				// If the remainder is 0, then current is not prime. Escape from the isPrime logic and go back to the main program.
 		je returnpoint
-
-		mov edx, 1				; Set ret to 1, signaling that eax is prime.
+		mov edx, 1				// Set ret to 1, signaling that eax is prime.
 		mov ret, edx
-
-		inc ebx					; Increment the divisor.
-
-		jmp primeLoop			; Continue the loop.
-
-	breakout:					; Escape point for the program.
-		nop						; No operation.
+		inc ebx					// Increment the divisor so we dont divide by the same number twice.
+		jmp primeLoop			// Jump back up to the loop start.
+	breakout:
 	}
 }
